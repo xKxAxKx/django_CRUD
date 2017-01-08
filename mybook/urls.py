2533cff9c783e9c50a506dac11026e71c11a7d08
xKxAxKx <pekepooooong@gmail.com>
2017-01-08 15:21:51 +0900

    imprlement Smart Phone API

--- mybook/urls.py
+++ mybook/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cms/', include('cms.urls', namespace='cms')),
+    url(r'^api/', include('api.urls', namespace='api')),
 ]
